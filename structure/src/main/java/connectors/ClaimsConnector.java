b7c9fe61724fbcf2e9579c7017a8654481031ee3
Joe <jordan@softwarium.net>
2017-12-08 19:34:44 +0300

    est

--- structure/src/main/java/connectors/ClaimsConnector.java
+++ structure/src/main/java/connectors/ClaimsConnector.java
@@ -10,7 +10,7 @@ public class ClaimsConnector {
 	public static final String HTTP_HEADER_COOKIE = "Cookie";
 	public static final String SPAPI_ITEM_UNIQUE_ID = "ETag";
 	
-	public String claims(final String url){
+	public String claims(String url){
 		ClaimsDialog claims = new ClaimsDialog(new Shell(Display.getDefault(), SWT.DIALOG_TRIM | SWT.RESIZE | SWT.SYSTEM_MODAL));
 		claims.open(url);
 		return claims.getCookie();
